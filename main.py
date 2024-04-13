d186ed0cf5685ed8fe5ae724bd28113e5d8a8800
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-04-13 11:43:44 +0700

    create get_youtube_data

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ def main():
     API_KEY = os.getenv('API_KEY')
 
     params = config()
-    print(API_KEY)
 
     channel_ids = [
-        'UClQ5yO2ZLZIs_Ii7J-C57ww', #AISPIK
-        'UCECdSNRGBOi5im0sfZvlkIg'  #fct-altai
+        'UClQ5yO2ZLZIs_Ii7J-C57ww',  # AISPIK
+        'UCECdSNRGBOi5im0sfZvlkIg'  # fct-altai
     ]
 
     data = get_youtube_data(API_KEY, channel_ids)
-    create_database('youtube', params)
-    save_data_to_database(data, 'youtube', params)
+    print(data)
+    # create_database('youtube', params)
+    # save_data_to_database(data, 'youtube', params)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -2,20 +2,33 @@ from typing import Any
 
 from googleapiclient.discovery import build
 
+
 def get_youtube_data(key: str, channel_ids: list[str]) -> list[dict[str, Any]]:
+    youtube = build('youtube', 'v3', developerKey=key)
 
-    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    data = []
+    data_videos = []
+    next_page_token = None
+    for channel_id in channel_ids:
+        channel_data = youtube.channels().list(part='snippet, statistics', id=channel_id).execute()
+        while True:
+            channel_video = youtube.search().list(part='snippet,id', channelId=channel_id, type='video', order='date',
+                                                  maxResults=50, pageToken=next_page_token).execute()
+            data_videos.extend(channel_video['items'])
 
-    request = youtube.channels().list(
-        part="snippet",
-        id="UClQ5yO2ZLZIs_Ii7J-C57ww"
-    )
-    response = request.execute()
+            next_page_token = channel_video.get('nextPageToken')
+            if not next_page_token:
+                break
+        data.append(
+            {'channel': channel_data['items'][0],
+             'videos': data_videos}
+        )
+    return data
 
 
-def create_database(database_name:str, params: dict) -> None:
+def create_database(database_name: str, params: dict) -> None:
     pass
 
 
-def save_data_to_database(data:list[dict[str, Any]], database_name:str, params: dict) -> None:
+def save_data_to_database(data: list[dict[str, Any]], database_name: str, params: dict) -> None:
     pass
